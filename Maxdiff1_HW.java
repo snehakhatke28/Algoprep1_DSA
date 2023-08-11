b95c59ac4cc81f72dfbae569745166e6524bcf06
snehakhatke28 <125118797+snehakhatke28@users.noreply.github.com>
2023-08-11 16:35:01 +0530

    Create Maxdiff1_HW.java

---
+++ Maxdiff1_HW.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(maxdifference_1(arr));
+    }
+    
+    public static int maxdifference_1(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return max - min;
+    }
+}
